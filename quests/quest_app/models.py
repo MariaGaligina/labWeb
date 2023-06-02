969fa60b66b45d0f7826343810bab2e1527c547a
Maria Galigina <mashagaligina2003@gmail.com>
2023-06-02 22:10:47 +0300

    created model and added path to graphic files

--- quests/quest_app/models.py
+++ quests/quest_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Quest(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    count_members = models.CharField(max_length=7)
+    splashsqreen = models.ImageField(upload_to="splashscreens/")
+    icon_people = models.ImageField(upload_to="icon/people/")
+    icon_type_quest = models.ImageField(upload_to="icon/type_quest/")
+    type_quest = models.CharField(max_length=30)
+    city = models.CharField(max_length=50)
+    subway = models.CharField(max_length=50)
+    raiting = models.IntegerField(default=0)
+    is_published = models.BooleanField(default=True)
